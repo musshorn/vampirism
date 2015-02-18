d2fb211912dd0fef811d209563d07bb21617899c
snippet <liam.sanders.91@gmail.com>
2015-02-18 23:54:40 +1100

    Working t1 worker training
    A large chunk of house1.lua is generic for all houses and can probably
    be moved to the generic building.lua file
    Worker ai still broken

--- game/dota_addons/vampirism/scripts/vscripts/buildings/building.lua
+++ game/dota_addons/vampirism/scripts/vscripts/buildings/building.lua
@@ -3,16 +3,5 @@ function TrainUnit( keys )
   table.insert(caster.queue, keys)
 end
 function Cancel( keys )
-  local caster = keys.caster
-  local ability = caster:GetAbilityByIndex(1) -- Dont know if this will always be true
-  if (ability:IsChanneling() == true) then
-    caster:Stop()
-    caster:RemoveAbility(ability:GetName())
-    caster:RemoveModifierByName(keys.cancel) 
-    print("Removed ability?")
-  end
-end
-function spawnWorkerT1( keys )
-  PrintTable(keys)
-  print("RED HOT MAYMAYS")
+  -- Soon (tm)
 end
--- game/dota_addons/vampirism/scripts/vscripts/buildings/house1.lua
+++ game/dota_addons/vampirism/scripts/vscripts/buildings/house1.lua
@@ -4,28 +4,45 @@ if House1 == nil then
 	House1.__index = House1
 end
 
-
 function House1:Init(unit)
 	local house1 = unit
-	house1.queue = {}
+	house1.queue = {} -- Queue of work to do, can be workers or research
+	local doingWork = false -- Flag to indicate if the queue is currently in use
 
 	Timers:CreateTimer(function()
 		if not unit:IsAlive() then
 			return nil
 		end
 
-		if table.getn(house1.queue) > 0 then
+		--Check if there is units in the queue and the queue is free
+		--Note the maximum displayable buffs seems to be 7, any more are not shown.
+		if table.getn(house1.queue) > 0  and doingWork == false then
 			local keys = house1.queue[1]
 			local caster =  keys.caster
 			local abilityName = keys.AddToQueue
+						
+			local ability = caster:FindAbilityByName(abilityName)
+			unitNameToBeCreated = ability
 
-			caster:AddAbility(abilityName)
+			ability:SetChanneling(true)
+			local spawnTime = ability:GetChannelTime()
+
+			doingWork = true
 			
-			ability = caster:FindAbilityByName(abilityName)
-			ability:SetLevel(1)
-			ability:CastAbility() 			
-			house1.queue[1] = nil
+			-- Create a timer on a delay to create the worker
+			Timers:CreateTimer(spawnTime, function()
+					local unit = Worker:Worker1(caster:GetAbsOrigin(), caster)
+					if unit.think then
+						unit:Think()
+					end
+					caster:RemoveModifierByName(ability:GetName())
+					ability:SetChanneling(false)
+					doingWork = false
+				return nil
+			end)
+			table.remove(house1.queue)
 		end
+
 		return .1
 	end)
 end
