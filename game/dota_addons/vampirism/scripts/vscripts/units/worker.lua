6cf263e7246fcaeb40ec5fce63cdb81428b851a6
snippet <liam.sanders.91@gmail.com>
2015-02-22 21:11:42 +1100

    Starting work on lumber harvesting
    Workers at trees will gain modifier stacks if they are standing still

--- game/dota_addons/vampirism/scripts/vscripts/units/worker.lua
+++ game/dota_addons/vampirism/scripts/vscripts/units/worker.lua
@@ -7,14 +7,32 @@ end
 function Worker:Worker1(vPos, hOwner)
   local worker = CreateUnitByName("worker_t1", vPos + VECTOR_BUMP, true, nil, nil, hOwner:GetTeam())
   worker:SetControllableByPlayer(hOwner:GetPlayerOwnerID() + 1, true)
-  worker:FindAbilityByName("harvest_t1"):SetLevel(1)
-  worker:SetMana(0)
+  --worker:FindAbilityByName("harvest_t1"):SetLevel(1)
   worker:SetHullRadius(8)
 
-  worker.maxmana = 10
-  worker.treepos = nil
+  worker.inTriggerZone = true -- Flag set true if worker is near trees
 
-  worker.think = true
+  worker.treepos = nil
+  worker.workTimer = DoUniqueString("WorkTimer")
+  worker.pos = worker:GetAbsOrigin()
+  worker.moving = false
+
+  Timers:CreateTimer(function()
+  	if worker.pos ~= worker:GetAbsOrigin() then
+  		local ability = worker:FindAbilityByName("harvest_channel")
+  		if (ability:IsChanneling()) then
+  			ability:SetChanneling(false)
+  		end
+  		
+  		worker.moving = true
+  		worker.pos = worker:GetAbsOrigin()
+  	else
+  		worker.moving = false
+  	end
+  	return 0.1
+  end)
+
+	-- Check if worker is stationary in a tree zone
 
 	function worker:Think()
 
@@ -25,10 +43,34 @@ function Worker:Worker1(vPos, hOwner)
 				return nil
 			end
 
-			--TODO: check if the player who owns the house is the same player who owns the worker.
+			-- Check if the worker is in the trigger zone and not moving
+			if (worker.inTriggerZone and worker.moving == false) then
+				local ability = worker:FindAbilityByName("harvest_channel")
+				if (ability:IsChanneling() == false) then
+					
+					ability:SetChanneling(true)
+					
+					local chopTime = ability:GetChannelTime()
+
+					-- Timer that increments the lumber stack count
+					Timers:CreateTimer(worker.workTimer,{
+							endTime = chopTime,
+							callback = function()
+								local carryTotal= worker:FindAbilityByName("carrying_lumber")
+								local currentLumber = worker:GetModifierStackCount("modifier_carrying_lumber", carryTotal)
+								worker:SetModifierStackCount("modifier_carrying_lumber", carryTotal, (currentLumber + 1))
+								ability:SetChanneling(false)
+
+								return nil
+							end})
+				end
+			end
+
+
+			--[[TODO: check if the player who owns the house is the same player who owns the worker.
 			if worker:GetMana() >= 1 then
 				if Entities:FindByModelWithin(nil, "models/house1.vmdl", worker:GetAbsOrigin(), 120) ~= nil then
-					print("found")
+					print("found house")
 					local pfxPath = string.format("particles/msg_fx/msg_%s.vpcf", "heal")
 					local pidx = ParticleManager:CreateParticle(pfxPath, PATTACH_ABSORIGIN_FOLLOW, worker)
 
@@ -53,36 +95,35 @@ function Worker:Worker1(vPos, hOwner)
 					worker:SetMana(0)
 					worker:MoveToPosition(worker.treepos)
 				end
-			end
+			end]]--
 
 			return .1
 		end)
 	end
 
   function worker:Harvest()
-  	--print(unit.harvesting)
-	local harvest = worker:FindAbilityByName("harvest_channel")
-	worker:CastAbilityNoTarget(harvest, worker:GetPlayerOwnerID())
-	--("npc_dota_creature", unit:GetAbsOrigin(), 10000000)
-	--[[
-	local vector = Vector(house:GetAbsOrigin())
-	print(vector)
-	unit:MoveToPosition(vector)]]
-  	-- body
-  end
-
+		local harvest = worker:FindAbilityByName("harvest_channel")
+		worker:CastAbilityNoTarget(harvest, worker:GetPlayerOwnerID())
+	  end
   return worker
 end
 
 function AtTree(keys)
+	print("At the tree")
 	local unit = keys.activator
-	unit:Harvest()
 	unit.treepos = unit:GetAbsOrigin()
-	local phase = unit:FindAbilityByName("harvest_phase")
-	--unit:CastAbilityNoTarget(phase, unit:GetPlayerOwnerID())
+	unit.inTriggerZone = true
+	unit:Think()
+end
+
+function LeftTree(keys)
+	print("Left the tree")
+	local unit = keys.activator
+	unit.inTriggerZone = false
 end
 
 function TreeLoop(keys)
+	print("In the meme loop?")
 	local unit = keys.caster
 	local harvest = unit:FindAbilityByName("harvest_channel")
 	local phase = unit:FindAbilityByName("harvest_phase")
