dcaea909564b33940843d69eaf7e007093ce770d
snippet <liam.sanders.91@gmail.com>
2015-06-27 17:50:49 +1000

    Closes #156

--- game/dota_addons/vampirism/scripts/vscripts/abilities.lua
+++ game/dota_addons/vampirism/scripts/vscripts/abilities.lua
@@ -286,10 +286,11 @@ function VerifyAttacker( keys )
 
   -- if you're attacking a unit that's not yours but in your base then its ok, otherwise stop the attacker
   if attackerPID ~= targetPID then
-    if target.inBase ~= BASE_OWNERSHIP[targetPID].BaseID then
-      attacker:Stop()
-      FireGameEvent( 'custom_error_show', { player_ID = attackerPID , _error = "You may only destroy other players buildings in your own base!" } )
-    end
+    if BASE_OWNERSHIP.targetPID ~= nil then
+      if target.inBase ~= BASE_OWNERSHIP[targetPID].BaseID then
+        attacker:Stop()
+        FireGameEvent( 'custom_error_show', { player_ID = attackerPID , _error = "You may only destroy other players buildings in your own base!" } )
+      end
   end
 end
 
