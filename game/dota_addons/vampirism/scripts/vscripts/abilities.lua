5a592c1a66799900b1135bf4f95a4337fca7041f
snippet <liam.sanders.91@gmail.com>
2015-06-28 10:05:49 +1000

    Fixes some base bugs

--- game/dota_addons/vampirism/scripts/vscripts/abilities.lua
+++ game/dota_addons/vampirism/scripts/vscripts/abilities.lua
@@ -285,11 +285,13 @@ function VerifyAttacker( keys )
   local targetPID = target:GetMainControllingPlayer()
 
   -- if you're attacking a unit that's not yours but in your base then its ok, otherwise stop the attacker
-  if attackerPID ~= targetPID then
-    if BASE_OWNERSHIP.targetPID ~= nil then
-      if target.inBase ~= BASE_OWNERSHIP[targetPID].BaseID then
-        attacker:Stop()
-        FireGameEvent( 'custom_error_show', { player_ID = attackerPID , _error = "You may only destroy other players buildings in your own base!" } )
+  if attackerPID < 8 then
+    if attackerPID ~= targetPID then
+      if  Bases.Owners[targetPID] ~= nil then
+        if target.inBase ~=  Bases.Owners[targetPID].BaseID then
+          attacker:Stop()
+          FireGameEvent( 'custom_error_show', { player_ID = attackerPID , _error = "You may only destroy other players buildings in your own base!" } )
+        end
       end
     end
   end
