605e7cf46f5ea949dd169044157b87c48d8ff787
snippet <liam.sanders.91@gmail.com>
2015-07-18 23:13:22 +1000

    Fixes slayer pool

--- game/dota_addons/vampirism/scripts/vscripts/hero_pool.lua
+++ game/dota_addons/vampirism/scripts/vscripts/hero_pool.lua
@@ -2,33 +2,32 @@ if SlayerPool == nil then
   SlayerPool = {}
 end
 
-Slayers_list = {}
-
 function SlayerPool:ActivatePool()
-  Timers:CreateTimer(0, --Start the timer for the slayer leveling pool
-    function()
-      for i=-1, 9 do
+  Timers:CreateTimer(50, function()
+      for i=0, 9 do
         if PlayerResource:GetTeam(i) == DOTA_TEAM_GOODGUYS then
-          if Slayers_list[i] ~= nil then
-            Slayers_list[i]:HeroLevelUp()
-            print('level slayer')
+          if SLAYERS[i] ~= nil then
+            if SLAYERS[i].handle ~= nil then
+              if SLAYERS[i].handle.inPool ~= nil then
+                SLAYERS[i].handle:HeroLevelUp(true)
+              end
+            end
           end
         end
       end
-      return 60
+      return 10
     end
   )
 end
 
 function OnStartTouch( trigger )
-  print('in pool')
-  if trigger.activator:GetName() == "npc_dota_hero_Invoker" then
-    Slayers_list[trigger.activator:GetMainControllingPlayer()] = trigger.activator
+  if trigger.activator:GetName() == "npc_dota_hero_invoker" then
+    trigger.activator.inPool = true
   end
 end
 
 function OnEndTouch( trigger )
-  if trigger.activator:GetName() == "npc_dota_hero_Invoker" then
-    Slayers_list[trigger.activator:GetMainControllingPlayer()] = nil
+  if trigger.activator:GetName() == "npc_dota_hero_invoker" then
+    trigger.activator.inPool = nil
   end
 end
