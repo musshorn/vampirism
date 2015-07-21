c958d0a4d2d41d70d41f8d813267c21da1ac62f4
space jam <space-jam-@users.noreply.github.com>
2015-07-21 18:46:35 +1000

    Fixes #191

--- game/dota_addons/vampirism/scripts/vscripts/vampirism.lua
+++ game/dota_addons/vampirism/scripts/vscripts/vampirism.lua
@@ -1,51 +1,51 @@
 print ('[VAMPIRISM] vampirism.lua' )
 
-VERSION_NUMBER = "0.01"                   -- Version number sent to panorama.
+VERSION_NUMBER = "0.02"                   -- Version number sent to panorama.
 
 ENABLE_HERO_RESPAWN = false              -- Should the heroes automatically respawn on a timer or stay dead until manually respawned
-UNIVERSAL_SHOP_MODE = false             -- Should the main shop contain Secret Shop items as well as regular items
-ALLOW_SAME_HERO_SELECTION = true        -- Should we let people select the same hero as each other
+UNIVERSAL_SHOP_MODE = false              -- Should the main shop contain Secret Shop items as well as regular items
+ALLOW_SAME_HERO_SELECTION = true         -- Should we let people select the same hero as each other
+ 
+HERO_SELECTION_TIME = 0.0                -- How long should we let people select their hero?
+PRE_GAME_TIME = 56.0                     -- How long after people select their heroes should the horn blow and the game start?
+POST_GAME_TIME = 60.0                    -- How long should we let people look at the scoreboard before closing the server automatically?
+TREE_REGROW_TIME = 60.0                  -- How long should it take individual trees to respawn after being cut down/destroyed?
 
-HERO_SELECTION_TIME = 0.0             -- How long should we let people select their hero?
-PRE_GAME_TIME = 56.0                    -- How long after people select their heroes should the horn blow and the game start?
-POST_GAME_TIME = 60.0                   -- How long should we let people look at the scoreboard before closing the server automatically?
-TREE_REGROW_TIME = 60.0                 -- How long should it take individual trees to respawn after being cut down/destroyed?
+GOLD_PER_TICK = 0                        -- How much gold should players get per tick?
+GOLD_TICK_TIME = 10000                   -- How long should we wait in seconds between gold ticks?
 
-GOLD_PER_TICK = 0                     -- How much gold should players get per tick?
-GOLD_TICK_TIME = 10000                      -- How long should we wait in seconds between gold ticks?
-
-RECOMMENDED_BUILDS_DISABLED = true     	-- Should we disable the recommened builds for heroes (Note: this is not working currently I believe)
+RECOMMENDED_BUILDS_DISABLED = true     	 -- Should we disable the recommened builds for heroes (Note: this is not working currently I believe)
 CAMERA_DISTANCE_OVERRIDE = 1500.0        -- How far out should we allow the camera to go?  1134 is the default in Dota
 
-MINIMAP_ICON_SIZE = 1                   -- What icon size should we use for our heroes?
-MINIMAP_CREEP_ICON_SIZE = 1             -- What icon size should we use for creeps?
-MINIMAP_RUNE_ICON_SIZE = 1              -- What icon size should we use for runes?
+MINIMAP_ICON_SIZE = 1                    -- What icon size should we use for our heroes?
+MINIMAP_CREEP_ICON_SIZE = 1              -- What icon size should we use for creeps?
+MINIMAP_RUNE_ICON_SIZE = 1               -- What icon size should we use for runes?
 
 RUNE_SPAWN_TIME = 120                    -- How long in seconds should we wait between rune spawns?
-CUSTOM_BUYBACK_COST_ENABLED = true      -- Should we use a custom buyback cost setting?
-CUSTOM_BUYBACK_COOLDOWN_ENABLED = true  -- Should we use a custom buyback time?
-BUYBACK_ENABLED = false                 -- Should we allow people to buyback when they die?
+CUSTOM_BUYBACK_COST_ENABLED = true       -- Should we use a custom buyback cost setting?
+CUSTOM_BUYBACK_COOLDOWN_ENABLED = true   -- Should we use a custom buyback time?
+BUYBACK_ENABLED = false                  -- Should we allow people to buyback when they die?
 
 DISABLE_FOG_OF_WAR_ENTIRELY = false      -- Should we disable fog of war entirely for both teams?
---USE_STANDARD_DOTA_BOT_THINKING = false  -- Should we have bots act like they would in Dota? (This requires 3 lanes, normal items, etc)
-USE_STANDARD_HERO_GOLD_BOUNTY = true    -- Should we give gold for hero kills the same as in Dota, or allow those values to be changed?
+--USE_STANDARD_DOTA_BOT_THINKING = false -- Should we have bots act like they would in Dota? (This requires 3 lanes, normal items, etc)
+USE_STANDARD_HERO_GOLD_BOUNTY = true     -- Should we give gold for hero kills the same as in Dota, or allow those values to be changed?
 
-USE_CUSTOM_TOP_BAR_VALUES = true        -- Should we do customized top bar values or use the default kill count per team?
+USE_CUSTOM_TOP_BAR_VALUES = true         -- Should we do customized top bar values or use the default kill count per team?
 TOP_BAR_VISIBLE = false                  -- Should we display the top bar score/count at all?
-SHOW_KILLS_ON_TOPBAR = true             -- Should we display kills only on the top bar? (No denies, suicides, kills by neutrals)  Requires USE_CUSTOM_TOP_BAR_VALUES
+SHOW_KILLS_ON_TOPBAR = true              -- Should we display kills only on the top bar? (No denies, suicides, kills by neutrals)  Requires USE_CUSTOM_TOP_BAR_VALUES
 
-ENABLE_TOWER_BACKDOOR_PROTECTION = false-- Should we enable backdoor protection for our towers?
-REMOVE_ILLUSIONS_ON_DEATH = false       -- Should we remove all illusions if the main hero dies?
-DISABLE_GOLD_SOUNDS = false             -- Should we disable the gold sound when players get gold?
-
-END_GAME_ON_KILLS = false               -- Should the game end after a certain number of kills?
-KILLS_TO_END_GAME_FOR_TEAM = 50         -- How many kills for a team should signify an end of game?
-
-USE_CUSTOM_HERO_LEVELS = true           -- Should we allow heroes to have custom levels?
-MAX_LEVEL = 200                         -- What level should we let heroes get to?
-USE_CUSTOM_XP_VALUES = true             -- Should we use custom XP values to level up heroes, or the default Dota numbers?
-
-WORKER_FACTOR = 4                       -- How many workers does a single worker count for. This can only be set once.
+ENABLE_TOWER_BACKDOOR_PROTECTION = false -- Should we enable backdoor protection for our towers?
+REMOVE_ILLUSIONS_ON_DEATH = false        -- Should we remove all illusions if the main hero dies?
+DISABLE_GOLD_SOUNDS = false              -- Should we disable the gold sound when players get gold?
+ 
+END_GAME_ON_KILLS = false                -- Should the game end after a certain number of kills?
+KILLS_TO_END_GAME_FOR_TEAM = 50          -- How many kills for a team should signify an end of game?
+ 
+USE_CUSTOM_HERO_LEVELS = true            -- Should we allow heroes to have custom levels?
+MAX_LEVEL = 200                          -- What level should we let heroes get to?
+USE_CUSTOM_XP_VALUES = true              -- Should we use custom XP values to level up heroes, or the default Dota numbers?
+ 
+WORKER_FACTOR = 4                        -- How many workers does a single worker count for. This can only be set once.
 FACTOR_SET = false
 
 LOW_PLAYER_MAP = true
@@ -272,12 +272,6 @@ function GameMode:OnDisconnect(keys)
       DISCONNECTED_PLAYERS[i] = true
     end
   end
-
-  --[[ Loop over playerIDs, check which playerID disconnected.
-  for i=-1,9 do
-    if PlayerResource:GetConnectionState(i) == 'dank'then end
-  end]]
-
 end
 -- The overall game state has changed
 function GameMode:OnGameRulesStateChange(keys)
